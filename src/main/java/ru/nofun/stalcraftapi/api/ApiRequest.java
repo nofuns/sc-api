cff6bbd137e57dfbb4828041e955d5dbd4c570ba
nofuns <ilyademin70@gmail.com>
2025-02-17 12:49:47 +0500

    Generic ApiRequest impl

--- src/main/java/ru/nofun/stalcraftapi/api/ApiRequest.java
+++ src/main/java/ru/nofun/stalcraftapi/api/ApiRequest.java
@@ -1,13 +1,19 @@
 package ru.nofun.stalcraftapi.api;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import ru.nofun.stalcraftapi.endpoints.Region;
 
 import java.net.http.HttpRequest;
 
 @AllArgsConstructor
 @Getter
-public class ApiRequest {
+public class ApiRequest <T> {
     private HttpRequest request;
+    private TypeReference<T> jsonFormat;
+
+    public ApiRequest(HttpRequest request) {
+        this.request = request;
+        this.jsonFormat = new TypeReference<T>(){};
+    }
 }
