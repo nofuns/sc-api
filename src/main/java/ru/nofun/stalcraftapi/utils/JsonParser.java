23071867b2fe88b39864cb385b422e0531bd4c9f
nofuns <ilyademin70@gmail.com>
2025-02-17 13:26:16 +0500

    Parsing by TypeReference method added

--- src/main/java/ru/nofun/stalcraftapi/utils/JsonParser.java
+++ src/main/java/ru/nofun/stalcraftapi/utils/JsonParser.java
@@ -1,13 +1,23 @@
 package ru.nofun.stalcraftapi.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 
 public class JsonParser {
     public static <T> T parse(String json, Class<T> clazz) {
         try {
             return new ObjectMapper().readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+    public static <T> T parse(String json, TypeReference<T> typeReference) {
+        try {
+            return new ObjectMapper().readValue(json, typeReference);
         } catch (JsonProcessingException e) {
             return null;
         }
