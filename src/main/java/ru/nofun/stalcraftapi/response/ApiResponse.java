5457c7c0f8b2c9d3bfb209dfcbe07b5733555984
nofuns <ilyademin70@gmail.com>
2025-02-17 12:50:31 +0500

    Generic ApiResponse impl

--- src/main/java/ru/nofun/stalcraftapi/response/ApiResponse.java
+++ src/main/java/ru/nofun/stalcraftapi/response/ApiResponse.java
@@ -1,14 +1,21 @@
-package ru.nofun.stalcraftapi.api;
+package ru.nofun.stalcraftapi.response;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import ru.nofun.stalcraftapi.utils.JsonParser;
 
 import java.net.http.HttpResponse;
 
 
-@AllArgsConstructor
-public class ApiResponse  {
+public class ApiResponse <T> {
     private final HttpResponse<String> response;
+    private final TypeReference<T> jsonFormat;
+
+    public ApiResponse(HttpResponse<String> response) {
+        this.response = response;
+        jsonFormat = new TypeReference<T>() {};
+    }
 
     public ResponseStatus status() {
         return ResponseStatus.fromValue(response.statusCode());
@@ -18,7 +25,7 @@ public class ApiResponse  {
         return response.body();
     }
 
-    public <T> T asSchemas(Class<T> jsonFormat) {
+    public T asSchemas() {
         return JsonParser.parse(response.body(), jsonFormat);
     }
 }
