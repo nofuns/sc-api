b3c4fb549cdf36b55d443a0962a903968ece3277
nofuns <ilyademin70@gmail.com>
2025-02-18 11:34:46 +0500

    ApiIMpl -> Api

--- src/main/java/ru/nofun/stalcraftapi/api/Api.java
+++ src/main/java/ru/nofun/stalcraftapi/api/Api.java
@@ -1,69 +1,21 @@
 package ru.nofun.stalcraftapi.api;
 
-import lombok.Getter;
-import lombok.Setter;
-import ru.nofun.stalcraftapi.endpoints.*;
-import ru.nofun.stalcraftapi.response.*;
-import ru.nofun.stalcraftapi.schemas.*;
+import lombok.NoArgsConstructor;
+import ru.nofun.stalcraftapi.endpoints.ApiMethod;
 
-public class Api {
-    private final ApiImpl apiImpl;
-    private final ApiRequestSender requestSender;
 
-    @Getter
-    @Setter
-    private Region region;
+@NoArgsConstructor
+public abstract class Api {
+    private static final int RATE_LIMIT = 200;
 
-    public Api(ApiImpl apiImpl, Region region) {
-        this.apiImpl = apiImpl;
-        this.region = region;
-        this.requestSender = new ApiRequestSender();
+    protected Version version = Version.PRODUCTION;
+
+    public static int getRateLimit() {
+        return RATE_LIMIT;
     }
 
-    public ApiResponse<PricesListing> lotPriceHistory(String itemId) {
-        return sendRequest(new AuctionHistory(itemId));
-    }
-
-    public ApiResponse<PricesListing> getLotPriceHistory(
-            String itemId, int limit, int offset, boolean additional) {
-
-        return sendRequest(new AuctionHistory(itemId, limit, offset, additional));
-    }
-
-    public ApiResponse<LotListing> getLotList(
-            String itemId, int limit, int offset, Order order, Sort sort, boolean additional) {
-
-        return sendRequest(new AuctionLots(itemId, limit, offset, order, sort, additional));
-    }
-
-    public ApiResponse<LotListing> getLotList(String itemId) {
-        return sendRequest(new AuctionLots(itemId));
-    }
-
-    public ApiResponse<EmissionResponse> getEmissionInfo() {
-        return sendRequest(new EmissionInfo());
-    }
-
-    public ApiResponse<ClanInfo> getClanInformation(String clanId) {
-        return sendRequest(new ClanInformation(clanId));
-    }
-
-    public ApiResponse<FullCharacterInfo> getCharacterInfo(String characterName) {
-        return sendRequest(new CharacterInfo(characterName));
-    }
-
-    private <T> ApiResponse<T> sendRequest(ApiMethod<T> method) {
-        return requestSender.send(
-                apiImpl.newRequest(method)
-                .region(region)
-                .build());
-    }
-
-    private void Test() {
-        ApiImpl apiImpl = new TokenApi("");
-        var request = apiImpl.newRequest(new AuctionHistory("")).region(region).build();
-        ApiRequestSender requestSender = new ApiRequestSender();
-        var response = requestSender.send(request);
-
+    public <T> ApiRequestBuilder<T> newRequest(ApiMethod<T> method) {
+        return new ApiRequestBuilder<>(method)
+                .version(version);
     }
 }
